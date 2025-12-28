81b2285682b70f0293f6763c87face181bac01ca
sehoon <ksstpgns1@gmail.com>
2025-12-29 01:30:05 +0900

    [feat] 로컬 소스 파일 추가

---
+++ sources/키미(김희재)_files/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_G4AFfCmEbWIvWKNFZUKihi5tHhcISQXwlUazK2nLvA4'] = {
+    config: {"token": "phc_G4AFfCmEbWIvWKNFZUKihi5tHhcISQXwlUazK2nLvA4", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "sessionRecording": false, "quotaLimited": ["recordings"], "heatmaps": false, "surveys": false, "productTours": false, "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();
